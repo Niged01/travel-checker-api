057ae9a7e9461fe24cb7e1c3a7c3d6ea2067697d
Niged01 <nigedoberer@gmail.com>
2022-11-03 09:33:10 +0000

    Refactored profile view

--- profiles/views.py
+++ profiles/views.py
@@ -42,8 +42,13 @@ class ProfileList(generics.ListAPIView):
 
 
 class ProfileDetail(APIView):
-    serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_at')
+    serializer_class = ProfileSerializer
 
     def get_object(self, pk):
         try:
